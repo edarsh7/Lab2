7e5db7bccb25dad88a2171a5a1572511768b3e78
edarsh7 <edarsh7@gmail.com>
2021-02-12 15:41:06 -0800

    mi

--- pintos/src/threads/semaphore.c
+++ pintos/src/threads/semaphore.c
@@ -74,7 +74,7 @@ semaphore_down(struct semaphore *sema)
     enum intr_level old_level = intr_disable();
     while (sema->value == 0) {
         //list_push_back(&sema->waiters, &thread_current()->elem);
-        list_insert_ordered(&sema->waiters, &thread_current()->elem, thread_prio_is_less);
+        list_insert_ordered(&sema->waiters, &thread_current()->elem, thread_prio_is_less, 0);
         thread_block();
     }
     sema->value--;
