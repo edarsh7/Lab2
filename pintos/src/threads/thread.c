968a014a222bbaf51646918a4e2d7f48f60d8437
edarsh7 <edarsh7@gmail.com>
2021-02-14 22:14:03 -0800

    mi

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -549,8 +549,6 @@ init_thread(struct thread *t, const char *name, int priority)
     strlcpy(t->name, name, sizeof t->name);
     t->stack = (uint8_t *) t + PGSIZE;
     t->priority = priority;
-    t->original = priority;
-    t->recieved = false;
     t->magic = THREAD_MAGIC;
     list_push_back(&all_list, &t->allelem);
 }
